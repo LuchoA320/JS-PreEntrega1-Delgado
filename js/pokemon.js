f8558db187c348a72776157b83f7047061c58330
Luciano Delgado <luchodelgado4803@gmail.com>
2023-09-02 15:42:22 -0300

    Agregadas nuevas funcionalidades de Energia y Salud, modificado aspecto en html y CSS, agregados iconos en botones, reemplazado charmander por torchic

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,75 +1,153 @@
-function bienvenida() {
-    
-const nombreUsuario = prompt("¿Como te llamas?");
+/* ======= Glosario =======
+  PP: Puntos de Poder
+  PS: Puntos de Salud
+  */
+
+
+// Prompt para pedir el nombre al usuario.
+const nombreUsuario = prompt("¡Hola! ¿Como te llamas?");
 alert("¡Bienvenido al mundo Pokemon " + nombreUsuario + "!");
-const menuJuego = prompt("¿Que deseas hacer? \n. Jugar. \n. Instrucciones de Juego.")
-switch (menuJuego) {
-    case "Jugar":
+
+function menuPokemon() {
+  let menuJuego;
+// Prompt del menu con 3 opciones.
+  while (
+    menuJuego != "Jugar" &&
+    menuJuego != "Instrucciones" &&
+    menuJuego != "Sorpresa"
+  ) {
+    menuJuego = prompt(
+      "¿Que deseas hacer? \n. Jugar. \n. Instrucciones. \n. Sorpresa."
+    );
+    switch (menuJuego) {
+      case "Jugar":
         break;
-    case "Instrucciones":
-        alert("Para subir de nivel tu Pokemon deberas entrenarlo, lo cual te costara energia que recuperaras alimentandolo. El nivel de tu Pokemon sera equivalente al daño que realizaras en tu contrincante.");
+      case "Instrucciones":
+        alert(
+          "Con la accion Atacar reduciras la salud (PS) de tu contrincante, pero le costara Puntos de Poder (PP) a tu Pokemon. El daño que causaras en tu oponente sera equivalente al nivel de tu Pokemon. \n Con la accion Entrenar, tu Pokemon subira de nivel a cambio de un Punto de Poder. Esto aumentara su Salud Maxima, asi como su capacidad de inflingir daño. No podras entrenar a tu Pokemon si se encuentra debilitado o su energia es demasiado baja.\n  La Baya Zanama aumentara los Puntos de Poder de tu Pokemon, y la Baya Aranja aumentara su salud."
+        );
         break;
-}
-}
-
-bienvenida()
-
-
-
-
-
-
-class Pokemon{
-    constructor(nombre, nivel, energia){
-        this.nombre = nombre;
-        this.nivel = nivel;
-        this.energia = energia;
+      case "Sorpresa":
+        window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        break;
+      default:
+        alert("Por favor, ingrese un comando valido.");
     }
-
-
-    // 1. Que suba de nivel del pokemon
-    // 2. Que me reste (1) de energia
-    // 3. Que no pueda entrenar si la energia del pokemon es igual o menor a 1
-    entrenar(){
-        if (this.energia <= 1) {
-            alert("No podes entrenar a " + this.nombre + " porque su energia es demasiado baja.");
-        } else {
-            this.nivel++;
-            this.energia--;
-            alert(this.nombre + " ha subido a nivel " + this.nivel);
-            alert("La energia de " + this.nombre + " es " + this.energia + ".");
-        }
-
-    }
-    // 1. Sube (1) de energia
-    // 2. Limitar la energia a 10
-    alimentar(){
-        if (this.energia < 10) {
-            this.energia ++;
-            alert("La energia de " + this.nombre + " ha subido a " + this.energia);
-        } else {
-            alert("La energia de " + this.nombre + " esta al maximo.");
-        }
-    }
-
-    // 1. Que le reste energia al pokemon enemigo
-    // 2. Que reste cantidad de energia segun el nivel del pokemon que ataca
-    atacar(objetivo){
-        objetivo.restarEnergia(this.nivel);
-    }
-
-    // 1. Que reste energia al pockemon segun la cantidad
-    // 2. Que si la energia es menor que 1, muestre un mensaje de que el pokemon esta incapacitado
-    restarEnergia (cantidad){
-        if (this.energia > 1){
-            this.energia = this.energia - cantidad;
-            alert ("Has utilizado placaje, la energia de " + this.nombre + " ha bajado a " + this.energia);
-        } else {
-            alert(this.nombre + " esta incapacitado :(");
-        }
-    }
+  }
 }
 
-const charmander = new Pokemon("Charmander", 1, 10);
-const treecko = new Pokemon("Treecko", 1, 10)
+menuPokemon();
 
+// 1. Parametros para la creacion de Nuevo Pokemon
+class Pokemon {
+  constructor(nombre, nivel, puntosPoder, salud, saludMaxima) {
+    this.nombre = nombre;
+    this.nivel = nivel;
+    this.puntosPoder = puntosPoder;
+    this.salud = salud;
+    this.saludMaxima = saludMaxima;
+  }
+
+  // 1. Que suba el nivel del pokemon.
+  // 2. Que me aumente la capacidad de PS Maxima
+  // 3. Que le reste (2) PP.
+  // 4. Que no pueda entrenar si los PP del pokemon son igual o menor a 1.
+  entrenar() {
+    if (this.puntosPoder <= 1 || this.salud <= 1) {
+      alert(
+        "No puedes entrenar a " +
+          this.nombre +
+          " porque sus puntos de poder son demasiado bajos o se encuentra debilitado. :("
+      );
+    } else {
+      this.nivel++;
+      this.saludMaxima = this.saludMaxima + 2;
+      this.puntosPoder = this.puntosPoder - 2;
+      alert("¡" + this.nombre + " ha subido a nivel " + this.nivel + "!");
+      alert(
+        "¡La salud maxima de " +
+          this.nombre +
+          " ha aumentado a " +
+          this.saludMaxima +
+          "!"
+      );
+      alert("Los puntos de poder de " + this.nombre + " han bajado a " + this.puntosPoder + ".");
+    }
+  }
+  // 1. Sube (1) de PP.
+  // 2. Limitar los PP maximos a 10.
+  alimentar() {
+    if (this.puntosPoder < 10) {
+      this.puntosPoder++;
+      alert(
+        "Los puntos de poder " + this.nombre + " han subido a " + this.puntosPoder + "."
+      );
+    } else {
+      alert("¡Los puntos de poder " + this.nombre + " estan al maximo!");
+    }
+  }
+
+  // 1. Que le reste PS al pokemon enemigo.
+  // 2. Que reste cantidad de PS segun el nivel del pokemon que ataca.
+  // 3. Que me reste 2 PP por ataque.
+  atacar(objetivo) {
+    if (this.puntosPoder <= 1 || this.salud <= 1) {
+      alert (this.nombre + " no puede atacar porque sus puntos de poder son demasiado bajos o se encuentra debilitado.")
+    } else {
+      objetivo.restarSalud(this.nivel);
+      this.puntosPoder = this.puntosPoder - 2;
+      alert(
+        "Los puntos de poder de " + this.nombre + " han bajado a " + this.puntosPoder + "."
+      );
+    }
+  }
+
+  // 1. Que reste PS al pokemon segun la cantidad.
+  // 2. Que si los PS son menores que 1, muestre un mensaje de que el pokemon esta debilitado.
+  restarSalud(cantidad) {
+    if (this.salud > 1) {
+      this.salud = this.salud - cantidad;
+      alert(
+        "Has utilizado placaje, la salud de " +
+          this.nombre +
+          " ha bajado a " +
+          this.salud +
+          "."
+      );
+    } else {
+      alert("¡" + this.nombre + " se ha debilitado!");
+    }
+  }
+
+  // 1. Que me sume 2 PS.
+  // 2. Que si los PS alcanzaron el nivel maximo, muestre un mensaje de salud maxima.
+  sumarSalud() {
+    if (this.salud < this.saludMaxima) {
+      this.salud = this.salud + 2
+      alert("La salud de " + this.nombre + " ha subido a " + this.salud);
+    } else {
+      alert("¡La salud de " + this.nombre + " esta al maximo!");
+    }
+    console.log(this.nombre + ": " + this.salud);
+  }
+
+  // ===== Boton Desabilitado, solo para desarrollo =====
+  // 1. Que reste la cantidad de salud siempre y cuando la vida sea mayor que 0.
+  // 2. Tope de 0.
+  dañar(cantidad) {
+    if (this.salud > 0) {
+      this.salud = this.salud - cantidad;
+    }
+
+    if (this.salud < 0) {
+      this.salud = 0;
+    }
+    console.log(this.nombre + ": " + this.salud);
+  }
+}
+
+// 1. Crear Pokemon nuevo utilizando sintaxis:
+// Const nombre = new Pokemon("Nombre", Nivel, PP, PS, PS Maximos.);
+
+const torchic = new Pokemon("Torchic", 1, 10, 10, 10);
+const treecko = new Pokemon("Treecko", 1, 10, 10, 10);
